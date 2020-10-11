4a681b4db5e513f462e9b8ab9612dc8983ea529e
Sandro Tsereteli <tsereteli.sandro0401@gmail.com>
2020-10-11 16:33:19 +0400

    exercise 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require("./controllers/blogsRouter")
 const usersRouter = require('./controllers/usersRouter')
+const loginRouter = require('./controllers/loginRouter')
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
 module.exports = app
